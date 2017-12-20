0c4fa90b647d4f2c008d666fa7675028a104e18b
Joel Gonzales <joelengt@gmail.com>
2017-12-19 19:20:24 -0500

    Updating

--- store.js
+++ store.js
@@ -1,6 +1,35 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { createLogger } from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
+
+/* set Provider */
+// import { Provider } from 'react-redux'
+
+// render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById('root')
+// )
+
+var middleware = [ thunkMiddleware ]
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger())
+}
+
+export const actionTypes = {
+  ADD: 'ADD',
+  REMOVE: 'REMOVE',
+  TICK: 'TICK',
+  NUMBER: 'NUMBER',
+  BOOLDATA: 'BOOLDATA',
+  GIFT: 'GIFT',
+  CART: 'CART',
+  CART_ADD: 'CART_ADD',
+  CART_REMOVE: 'CART_REMOVE',
+  CART_UPDATE: 'CART_UPDATE'
+}
 
 const exampleInitialState = {
   lastUpdate: 0,
@@ -12,51 +41,122 @@ const exampleInitialState = {
   gift: ''
 }
 
-export const actionTypes = {
-  ADD: 'ADD',
-  REMOVE: 'REMOVE',
-  TICK: 'TICK',
-  NUMBER: 'NUMBER',
-  BOOLDATA: 'BOOLDATA',
-  CART: 'CART',
-  GIFT: 'GIFT'
+const initialStatePost = {
+  isFetching: false,
+  didInvalidate: false,
+  items: []
 }
 
+
 // REDUCERS
+export const posts = (state = initialStatePost, action) => {
+  switch (action.type) {
+    case actionTypes.CART_ADD:
+      return {
+        ...state,
+        didInvalidate: true
+      }
+
+    case actionTypes.CART_REMOVE:
+      return {
+        ...state,
+        isFetching: true,
+        didInvalidate: false
+      }
+
+    case actionTypes.CART_UPDATE:
+      return {
+        ...state,
+        isFetching: false,
+        didInvalidate: false,
+        items: action.posts,
+        lastUpdated: action.receivedAt
+      }
+
+    default:
+      return state
+  }
+}
+
 export const reducer = (state = exampleInitialState, action) => {
   switch (action.type) {
     case actionTypes.TICK:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         lastUpdate: action.ts,
         light: !!action.light
-      })
+      }
+
     case actionTypes.ADD:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         count: state.count + 1
-      })
+      }
+
     case actionTypes.REMOVE:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         count: state.count - 1
-      })
+      }
+
     case actionTypes.NUMBER:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         value: state.value + 5
-      })
+      }
+
     case actionTypes.BOOLDATA:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         turning: !state.turning
-      })
+      }
+
     case actionTypes.CART:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         cart: action.cart
-      })
+      }
+
     case actionTypes.GIFT:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         gift: action.text
-      })
-    default: return state
+      }
+
+    default:
+      return state
   }
 }
+
+export const cart = (state = exampleInitialState, action) => {
+  switch (action.type) {
+    case actionTypes.CART_ADD:
+      return {
+        ...state,
+        turning: !state.turning
+      }
+
+    case actionTypes.CART_REMOVE:
+      return {
+        ...state,
+        cart: action.cart
+      }
+
+    case actionTypes.CART_UPDATE:
+      return {
+        ...state,
+        [action.subreddit]: posts(state[action.subreddit], action)
+      }
+
+    default:
+      return state
+  }
+}
+
+const rootReducer = combineReducers({
+  reducer,
+  cart
+})
 
 // ACTIONS
 export const serverRenderClock = (isServer) => dispatch => {
@@ -91,23 +191,26 @@ export const updateGift = (text) => dispatch => {
   return dispatch({ type: actionTypes.GIFT, text: text })
 }
 
-export const updateCoupon = (text) => dispatch => {
+export const updateCoupon = (text) => (dispatch, getState) => {
   return dispatch({ type: actionTypes.CART })
 }
 
-export const initStore = (initialState = exampleInitialState) => {
-  return createStore(reducer, initialState, composeWithDevTools(applyMiddleware(thunkMiddleware)))
+/* actions set */
+export const getText = text => ({
+  type: actionTypes.GIFT,
+  text
+})
+
+export const fetchData = text => dispatch => {
+  return dispatch(getText(text))
 }
 
+export const fetchMainData = text => dispatch => {
+  dispatch(getText(text))
+  return { status: 'ok?' }
+}
 
-
-// plus +1 (or other number) +
-// switch true or false +
-// update the current number +
-// update the current text +
-
-// product example + cart
-
-// cart +1, -1, update number, remove,
-
-
+/* Init Store */
+export const initStore = (initialState = exampleInitialState) => {
+  return createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+}
